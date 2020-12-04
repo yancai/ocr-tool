03220c7d4d6d68bd3d444080f8de467e92035182
yancai <yancai915@gmail.com>
2020-12-04 10:07:36 +0800

    提交OCR转换服务代码

---
+++ settings.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+# 访问百度OCR接口的ak sk
+AK = "akxxx"
+SK = "skxxx"
+
+# 图片输入目录
+INPUT_DIR = "./input"
+
+# 支持识别的扩展名
+EXTS = (
+    ".jpg",
+    ".jpeg",
+    ".png",
+    ".bmp",
+)
+
+if __name__ == "__main__":
+    pass
